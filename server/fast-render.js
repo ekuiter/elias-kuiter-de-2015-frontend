186d8c074c71df50243524bb58db7e99d23e08b8
Elias Kuiter <info@elias-kuiter.de>
2015-09-21 23:28:58 +0200

    FastRender Fix

--- server/fast-render.js
+++ server/fast-render.js
@@ -10,6 +10,7 @@ Meteor.startup(function() {
 
   FastRender.route("/:categorySlug/:projectSlug", function(params) {
     this.subscribe("projectsInCategory", params.categorySlug);
+    this.subscribe("imagesForCategory", params.categorySlug);
     this.subscribe("project", params.categorySlug, params.projectSlug);
     this.subscribe("imagesForProject", params.categorySlug, params.projectSlug);
   });
