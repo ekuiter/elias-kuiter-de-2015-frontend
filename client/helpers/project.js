ba08700ab9aea887245c9c93d2979933af16eefd
Elias Kuiter <info@elias-kuiter.de>
2015-09-24 14:34:19 +0200

    Inline widgets in descriptions

--- client/helpers/project.js
+++ client/helpers/project.js
@@ -23,6 +23,42 @@ Template.projectRoute.helpers({
   projectPath: projectPath
 });
 
+Template.projectDescription.helpers({
+  descriptionHtml: function() {
+    function parse(html) {
+      var startTag = "{{", endTag = "}}";
+
+      function tagIndex(html) {
+        var startIndex = html.indexOf(startTag);
+        if (startIndex === -1) return null;
+        var endIndex = html.indexOf(endTag);
+        if (endIndex === -1) return null;
+        return { start: startIndex, end: endIndex };
+      }
+
+      function renderWidget(options) {
+        if (options[0] === "action") {
+          return Blaze.toHTMLWithData(Template.action, {
+            title: options[1], url: "http://" + options[2].replace("_DOT_", "."), target: "_blank"
+          });
+        } else if (options[0] === "githubAction")
+          return Blaze.toHTMLWithData(Template.githubAction, { githubRepository: options[1] });
+        else
+          return "";
+      }
+
+      var tag = tagIndex(html);
+      if (!tag) return html;
+      var before = html.substring(0, tag.start);
+      var after = html.substring(tag.end + endTag.length);
+      var between = html.substring(tag.start + startTag.length, tag.end);
+      var options = JSON.parse(between.replace(/&quot;/g, "\""));
+      return before + renderWidget(options) + parse(after);
+    }
+    return parse(this.descriptionHtml);
+  }
+});
+
 Template.projectImage.helpers({
   imageUrl: function() {
     var imageNumber = App.helpers.getImageNumber();
